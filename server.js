6889411a3f356e5380487c2f9a0d28e569a90ea5
maxicoder <palombamassimo80@gmail.com>
2015-05-18 14:35:35 +0100

    Update server.js
    added dynamic port

--- server.js
+++ server.js
@@ -61,8 +61,10 @@ var server = http.createServer(function(request, response) {
     serveStatic(response, cache, absPath);
 });
 
-server.listen(3000, function() {
-    console.log("Server listening on port 3000.");
+port = Number(process.env.PORT || 3000);
+
+server.listen(port, function() {
+    console.log("Server listening on port " + port + ".");
     console.log("");
 });
 
